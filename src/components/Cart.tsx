6f026597c403cf2cf82a154878602ec09cb212e8
Hyuksang-Oh <iuesver@gmail.com>
2022-08-15 22:24:12 +0900

    누락되었던 총 가격란 업데이트

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -20,20 +20,37 @@ type item = {
     title : string;
     price : number;
     image : string;
+    count : number;
 }
 
 export default function Cart() {
     const [cart, setCart] = useState(cartItemFromLocalStorage);
     // 추후 item 개별 페이지에서 addToCartButton 클릭시 localstorage의 CART_ITEM 배열에 { id : n, count : 1 } 식으로 추가 예정
     const [count, setCount] = useState(1);
+    const [total, setTotal] = useState(0)
 
     useEffect(() => {
-        localStorage.setItem(CART_ITEM,JSON.stringify(cart));
-    },[cart]);
+        return () => {
+            localStorage.setItem(CART_ITEM,JSON.stringify(cart));
+        }
+    });
+
+    useEffect(() => {
+        setTotal(calculateTotalPrice)
+    },[])
 
     const removeFromCart = (id : number) => {
         const temp = cart.filter((item : item) => item.id !== id);
         setCart(temp);
+    }
+    
+    const calculateTotalPrice = () : number => {
+        let price = 0;
+        cart.reduce((acc : number, cur : item) => {
+            acc += cur.price;
+            return price = acc;
+        },0);
+        return price;
     }
 
     return(
@@ -48,11 +65,24 @@ export default function Cart() {
                         <img src={item.image} alt="" />
                         <div className="itemInfo">
                             <h2>{item.title}</h2>
-                            <p>{item.price * count}</p>
-                            <button onClick={() => count === 1 ? removeFromCart(item.id) : setCount(count - 1)}>-</button>{count}<button onClick={() => setCount(count + 1)}>+</button>
+                            <p>$ {(item.price * count).toFixed(2)}</p>
+                            <button onClick={() => {
+                                count === 1 ? removeFromCart(item.id) 
+                                : setCount(count - 1)
+                                setTotal(total - (item.price * count))
+                            }}>-</button>
+                            {count}
+                            <button onClick={() => {
+                                setCount(count + 1)
+                                setTotal(total + (item.price * count))
+                            }}>+</button>
                         </div>
                     </div>
                 ))}
+            </div>
+            <div className="cartTotalPrice">
+                    <label htmlFor="totalPrice">총 : ${total.toFixed(2)} </label>
+                    <button id="totalPrice"> 구매하기 </button>
             </div>
         </div>
     )
