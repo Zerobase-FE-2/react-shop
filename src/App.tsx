bd27d07fef77c6d016e47ceeb1e66950ba9c8159
woong <inwoong100@gmail.com>
2022-08-16 23:14:43 +0900

    feature: create signup page 1. signup page 따로 만들기 2. react-hook-form 사용하여 form, error handling 3. throw error, catch하여 alert 하기 4. error code에 대한 message 구체화 하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import Login from './components/Login';
+import { Routes, Route } from 'react-router-dom';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Routes>
+        <Route path="/signin" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
 import tw from 'tailwind-styled-components';
+import { Link } from 'react-router-dom';
 import { LockClosedIcon } from '@heroicons/react/solid';
 import githubIcon from '../assets/icon/iconGithub.svg';
 import googleIcon from '../assets/icon/iconGoogle.svg';
 
 import * as AuthService from '../service/auth';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+interface LogInInputs {
+  email: string;
+  password: string;
+}
 
 const Login = () => {
   const LoginSection = tw.section`
@@ -13,45 +20,33 @@ const Login = () => {
   const LoginTitle = tw.h2`
   mt-6 text-center text-3xl font-extrabold text-gray-900
   `;
-  const EmailInput = tw.input`
+  const Input = tw.input`
   appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm
   `;
 
-  const PasswordInput = tw.input`
-  appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm
-  `;
-
   const SignInBtn = tw.button`
-  group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
+  group relative w-1/2 ml-1 flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
   `;
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [signUp, setSignUp] = useState(false);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value, checked } = event.target;
-    switch (name) {
-      case 'email':
-        setEmail(value);
-        break;
+  const SignUpBtn = tw.div`
+  group relative w-1/2 flex mr-1 justify-center  border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
+  `;
 
-      case 'password':
-        setPassword(value);
-        break;
-      case 'signUp':
-        setSignUp(checked);
-        break;
-    }
-  };
+  const Error = tw.p`
+  text-red-500
+  `;
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LogInInputs>();
+
+  const onSubmit: SubmitHandler<LogInInputs> = async (data, e) => {
+    const { email, password } = data;
     try {
-      if (signUp) {
-        AuthService.signUp(email, password);
-      } else {
-        AuthService.signIn(email, password);
-      }
+      e?.target.reset();
+      await AuthService.signIn(email, password);
     } catch (error) {
       if (typeof error == 'string') {
         alert(error);
@@ -88,45 +83,47 @@ const Login = () => {
   return (
     <LoginSection>
       <div className="max-w-md w-full space-y-8">
-        <LoginTitle>{signUp ? 'Sign Up' : 'Sign In'} to React-shop</LoginTitle>
-
+        <LoginTitle>Sign in to React-shop</LoginTitle>
         <form
           action="submit"
           className="mt-8 space-y-6"
           method="POST"
-          onSubmit={handleSubmit}
+          onSubmit={handleSubmit(onSubmit)}
         >
           <input type="hidden" name="remember" defaultValue="true" />
           <div>
             <label htmlFor="email" className="sr-only">
               Email
             </label>
-            <EmailInput
+            <Input
               type="email"
-              name="email"
-              autoComplete="email"
-              required
               placeholder="What is your email"
-              onChange={handleChange}
-              value={email}
+              {...register('email', { required: true })}
             />
+            <Error>{errors.email?.message}</Error>
           </div>
           <div>
             <label htmlFor="password" className="sr-only">
               Password
             </label>
-            <PasswordInput
+            <Input
               type="password"
-              name="password"
               placeholder="Write your password"
-              autoComplete="current-password"
-              required
-              onChange={handleChange}
-              value={password}
+              {...register('password', { required: true })}
             />
+            <Error>{errors.password?.message}</Error>
           </div>
 
-          <div>
+          <div className="flex">
+            <SignUpBtn>
+              <Link
+                className="w-full h-full py-2 px-4 flex justify-center"
+                to="/signup"
+              >
+                Sign Up
+              </Link>
+            </SignUpBtn>
+
             <SignInBtn type="submit">
               <span className="absolute left-0 inset-y-0 flex items-center pl-3">
                 <LockClosedIcon
@@ -134,28 +131,16 @@ const Login = () => {
                   aria-hidden="true"
                 />
               </span>
-              {signUp ? 'Sign Up' : 'Sign In'}
+              Sign In
             </SignInBtn>
           </div>
           <div className="flex items-center justify-around">
-            {signUp && (
-              <span className="text-lg font-bold">Sign Up with SNS</span>
-            )}
             <div className="btn btn-ghost" onClick={handleGithub}>
               <img src={githubIcon} className="h-10 w-10" alt="Icon_github" />
             </div>
             <div className="btn btn-ghost" onClick={handleGoogle}>
               <img src={googleIcon} className="h-10 w-10" alt="Icon_google" />
             </div>
-          </div>
-          <div className="flex items-center justify-end">
-            <label htmlFor="signUp">If you don't have ID? </label>
-            <input
-              type="checkbox"
-              name="signUp"
-              onChange={handleChange}
-              checked={signUp}
-            />
           </div>
         </form>
       </div>
