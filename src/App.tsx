ce6184e54da8d2731d1267b066ef85ec71ee15bf
Hyuksang-Oh <iuesver@gmail.com>
2022-08-19 02:58:48 +0900

    핵심 : localStorage환경에서 redux환경으로 변경 1. store 추가 2. 장바구니 redux store에 따른 렌더링 3. react-redux, redexjs/toolkit, swr 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import Login from './components/Login';
+import Cart from './components/Cart';
+import { Provider } from 'react-redux'
+import { store } from './store/store'
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Provider store={store}>
+        <Cart />
+      </Provider>
     </div>
   );
 }
