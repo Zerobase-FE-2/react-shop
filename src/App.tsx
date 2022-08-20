efe1d422a10e4e86844be7b3be9b0748e4199535
woong <inwoong100@gmail.com>
2022-08-20 16:50:22 +0900

    feature: 자동으로 슬라이드 넘기기
    1. 자동으로 슬라이드 넘어가는 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import Login from './components/Login';
 import { Routes, Route } from 'react-router-dom';
 import SignUp from './components/SignUp';
+import Index from './components/Index';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="signup" element={<SignUp />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={<Index />} />
       </Routes>
     </div>
   );
