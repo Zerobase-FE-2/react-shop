b58a5c5376b013cbefff0112751d5e43684dcb53
Hyuksang-Oh <iuesver@gmail.com>
2022-08-27 15:46:14 +0900

    1. 장바구니 반응형 CSS 추가 2. redux-persist 설치

--- src/reducers/combReducer.tsx
+++ src/reducers/combReducer.tsx
@@ -1,9 +1,17 @@
 import { combineReducers } from "redux"
 import itemFilter from './itemFilter'
 import cartReducer from './cartReducer'
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key : 'root',
+  storage,
+};
 
 const reducers = combineReducers({
-  itemList: itemFilter, cart: cartReducer,
+  itemList: itemFilter,
+  cart: cartReducer,
 });
 
-export default reducers;
+export default persistReducer(persistConfig, reducers);
