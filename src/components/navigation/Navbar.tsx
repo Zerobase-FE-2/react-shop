b6508a8f323faacfa3f97f6b3edf68683f58bae2
Hyuksang-Oh <iuesver@gmail.com>
2022-08-31 02:29:59 +0900

    NavBar fixed속성으로 인해 BreadCrumb 숨겨졌던 문제 해결

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -3,7 +3,7 @@ import {Link, Outlet} from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import tw from 'tailwind-styled-components';
 import SearchBar from './SearchBar';
-import { ShoppingBagIcon, SunIcon, MoonIcon } from '@heroicons/react/outline'
+import { ShoppingBagIcon, SunIcon, MoonIcon, SearchIcon } from '@heroicons/react/outline'
 import localStorage from 'redux-persist/es/storage';
 
 type item = {
@@ -29,7 +29,7 @@ export default function Navbar() {
     return acc;
   },0)
   const NavBar = tw.nav`
-  w-full h-16 p-2 bg-white dark:bg-gray-900 flex justify-between shadow-lg z-10 fixed
+  w-full h-16 p-2 bg-white dark:bg-gray-900 flex justify-between shadow-lg sticky
   `
   const LeftBar = tw.div`
   flex items-center
@@ -38,10 +38,10 @@ export default function Navbar() {
   flex
   `
   const HomeBtn = tw.h1`
-  inline text-black dark:text-white text-sm md:text-base lg:text-lg font-bold px-3 py-1
+  text-black dark:text-white text-sm md:text-base lg:text-lg font-bold px-3 py-1
   `
   const NavBtn = tw.div`
-  relative text-black dark:text-white invisible md:visible md:text-sm lg:text-base font-semibold px-3 py-2 mx-2 hover:bg-gray-200 dark:hover:bg-gray-700 hover:rounded-lg
+  relative text-black dark:text-white hidden md:block md:text-sm lg:text-base font-semibold px-3 py-2 mx-2 hover:bg-gray-200 dark:hover:bg-gray-700 hover:rounded-lg
   `
   const CartCount = tw.div`
   absolute flex justify-center items-center text-white top-2 left-9 w-4 h-4 text-xs border-2 border-red-500 bg-red-500 rounded-full
@@ -57,12 +57,22 @@ export default function Navbar() {
           <Link to='/login'><NavBtn>로그인</NavBtn></Link>
       </LeftBar>
       <RightBar>
-          <NavBtn onClick={() => setTheme(colorTheme)}>{colorTheme === 'light' ? <SunIcon className='w-7 h-7 mx-2' /> : <MoonIcon className='w-7 h-7 mx-2' />}</NavBtn>
-          <SearchBar/>
+          <NavBtn className="block" onClick={() => setTheme(colorTheme)}>{colorTheme === 'light' ? <SunIcon className='w-7 h-7 mx-2' /> : <MoonIcon className='w-7 h-7 mx-2' />}</NavBtn>
+          <SearchBar />
+          <NavBtn className="block md:hidden"><SearchIcon className='w-7 h-7 mx-2' onClick={() => {
+            const search = document.querySelector('input');
+            if(search?.classList.contains('hidden')){
+              search?.classList.remove('hidden');
+              search?.classList.add('block');
+            } else {
+              search?.classList.remove('block');
+              search?.classList.add('hidden');
+            }
+          }} /></NavBtn>
           <Link to='/cart'>
-            <NavBtn>
+            <NavBtn className="block">
               <ShoppingBagIcon className='w-7 h-7 mx-2' />
-              <CartCount>{count}</CartCount>
+              <CartCount className="blcok">{count}</CartCount>
             </NavBtn>
           </Link>
       </RightBar>
