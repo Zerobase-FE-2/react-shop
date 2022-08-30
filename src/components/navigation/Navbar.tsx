fb8f7026cb916d394ae28786c2429bd63cf5a29a
Hyuksang-Oh <iuesver@gmail.com>
2022-08-31 00:38:03 +0900

    1. 전체적인 반응형 스타일 추가 2. tailwind CSS를 통한 다크모드 구현 3. redux.state를 참고하여 navBar에서 현재 장바구니 갯수 확인기능 구현

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,11 +1,35 @@
+import { useState, useEffect } from 'react';
 import {Link, Outlet} from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import tw from 'tailwind-styled-components';
 import SearchBar from './SearchBar';
 import { ShoppingBagIcon, SunIcon, MoonIcon } from '@heroicons/react/outline'
+import localStorage from 'redux-persist/es/storage';
+
+type item = {
+  id : number;
+  title : string;
+  price : number;
+  image : string;
+  count : number;
+};
 
 export default function Navbar() {
+  const [theme, setTheme] = useState('dark');
+  const colorTheme = theme === 'dark' ? 'light' : 'dark';
+  useEffect(() => {
+    const root = window.document.documentElement;
+    root.classList.remove(colorTheme);
+    root.classList.add(theme);
+    localStorage.setItem('theme', theme)
+  },[theme, colorTheme])
+  const condition = useSelector((state : any) => state.cart);
+  const count = condition.reduce((acc : number, cur : item) => {
+    acc += cur.count;
+    return acc;
+  },0)
   const NavBar = tw.nav`
-  w-full h-16 p-2 bg-gray-900 flex justify-between
+  w-full h-16 p-2 bg-white dark:bg-gray-900 flex justify-between shadow-lg z-10 fixed
   `
   const LeftBar = tw.div`
   flex items-center
@@ -14,13 +38,16 @@ export default function Navbar() {
   flex
   `
   const HomeBtn = tw.h1`
-  inline text-white text-sm md:text-base lg:text-lg font-bold px-3 py-1
+  inline text-black dark:text-white text-sm md:text-base lg:text-lg font-bold px-3 py-1
   `
   const NavBtn = tw.div`
-  text-white invisible md:visible md:text-sm lg:text-base font-semibold px-3 py-2 mx-2 hover:bg-gray-700 hover:rounded-lg
+  relative text-black dark:text-white invisible md:visible md:text-sm lg:text-base font-semibold px-3 py-2 mx-2 hover:bg-gray-200 dark:hover:bg-gray-700 hover:rounded-lg
+  `
+  const CartCount = tw.div`
+  absolute flex justify-center items-center text-white top-2 left-9 w-4 h-4 text-xs border-2 border-red-500 bg-red-500 rounded-full
   `
   return (
-  <div>
+  <>
     <NavBar>
       <LeftBar>
           <Link to='/'><HomeBtn>React Shop</HomeBtn></Link>
@@ -30,12 +57,17 @@ export default function Navbar() {
           <Link to='/login'><NavBtn>로그인</NavBtn></Link>
       </LeftBar>
       <RightBar>
-          <NavBtn><SunIcon className='w-7 h-7 mx-2' /></NavBtn>
+          <NavBtn onClick={() => setTheme(colorTheme)}>{colorTheme === 'light' ? <SunIcon className='w-7 h-7 mx-2' /> : <MoonIcon className='w-7 h-7 mx-2' />}</NavBtn>
           <SearchBar/>
-          <Link to='/cart'><NavBtn><ShoppingBagIcon className='w-7 h-7 mx-2' /></NavBtn></Link>
+          <Link to='/cart'>
+            <NavBtn>
+              <ShoppingBagIcon className='w-7 h-7 mx-2' />
+              <CartCount>{count}</CartCount>
+            </NavBtn>
+          </Link>
       </RightBar>
     </NavBar>
     <Outlet />
-  </div>
+  </>
   )
 }
--- src/components/productShow/AccessoryPage.tsx
+++ src/components/productShow/AccessoryPage.tsx
@@ -5,12 +5,12 @@ import BreadCrumb from '../navigation/BreadCrumb'
 export default function AccessoryPage() {
 
   const Title = tw.h1`
-  text-3xl font-bold p-6 text-center
+  text-3xl font-bold p-6 text-center text-black dark:text-white
   `
 
   return (
-    <div>
-      <div className='m-4'>
+    <div className='h-fit lg:min-h-screen bg-white h-screen dark:bg-gray-800'>
+      <div className='p-4'>
         <BreadCrumb />
       </div>
       <Title>악세서리</Title>
