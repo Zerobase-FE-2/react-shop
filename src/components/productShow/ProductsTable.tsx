6f3f7ab46aa0fe71c2f53cb2ab5ac311739e090f
woong <inwoong100@gmail.com>
2023-01-03 23:05:27 +0900

    refactor(main): refactor mainpage
    set skeleton ui, divide by each component

--- src/components/productShow/ProductsTable.tsx
+++ src/components/productShow/ProductsTable.tsx
@@ -1,35 +1,31 @@
-import { Link } from 'react-router-dom';
-import { useAppSelector } from '../../hooks/rtkHooks';
-import { getAccesoryProducts } from '../../reducers/productSlice';
+import { Product } from '../../reducers/productSlice';
+import ProductItemSkeleton from './Loading/ProductItemSkeleton';
+import ProductItem from './ProductItem';
 
-interface ProductsTable {
-  category: string;
+interface ProductsTableProps {
+  products: Product[];
+  loading: boolean;
 }
 
-export default function ProductsTable({ category }: ProductsTable) {
-  const products = useAppSelector(getAccesoryProducts);
+export default function ProductsTable({
+  products,
+  loading,
+}: ProductsTableProps) {
   return (
-    <div className="grid pb-20 md:grid-cols-2 lg:grid-cols-4">
-      {products.map((prodcut: any) => (
-        <div
-          key={prodcut.id}
-          className="m-2 rounded-lg border dark:border-none"
-        >
-          <Link to={`/${prodcut.id}`}>
-            <figure className="flex h-80 items-center justify-center rounded-t-lg bg-white">
-              <img
-                src={prodcut.image}
-                className="img-primary"
-                alt={prodcut.title}
-              />
-            </figure>
-            <div className="flex h-36 flex-col items-start justify-between overflow-auto bg-gray-200 p-4 text-black dark:rounded-b-lg dark:bg-gray-700 dark:text-gray-400">
-              <span className="text-start font-semibold">{prodcut.title}</span>
-              <span className="font-semibold">${prodcut.price}</span>
-            </div>
-          </Link>
+    <>
+      {loading ? (
+        <div className={`grid pb-20 md:grid-cols-2 lg:grid-cols-4`}>
+          {Array.from({ length: 8 }, (_, idx) => idx).map((idx) => (
+            <ProductItemSkeleton idx={idx} />
+          ))}
         </div>
-      ))}
-    </div>
+      ) : (
+        <div className={`grid gap-10 pb-20 lg:grid-cols-2 xl:grid-cols-4`}>
+          {products.map((product: Product) => (
+            <ProductItem product={product} />
+          ))}
+        </div>
+      )}
+    </>
   );
 }
