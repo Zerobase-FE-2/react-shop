d39bc707a2e126675eecb49439400798e792cf31
woong <inwoong100@gmail.com>
2023-01-09 23:07:19 +0900

    refactor:set lazy-load for mainpage Images

--- src/components/productShow/ProductItem.tsx
+++ src/components/productShow/ProductItem.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 import { Link } from 'react-router-dom';
 import { Product } from '../../types';
 
@@ -12,14 +14,15 @@ export default function ProductItem({ product }: ProductItemProps) {
       key={product.id}
       className="group m-2 rounded-lg border-2 shadow-xl transition-opacity hover:opacity-60 dark:border-none"
     >
-      <Link to={`/${product.id}`}>
-        <figure className="hover: flex h-80 items-center justify-center rounded-t-lg bg-white">
-          <img
-            src={product.image}
-            className="img-primary transition group-hover:scale-110"
+      <Link className="h-full w-full" to={`/${product.id}`}>
+        <div className="group flex h-80 items-center justify-center rounded-t-lg bg-white text-center transition-all">
+          <LazyLoadImage
+            className="img-primary h-60 transition-all duration-300 ease-out group-hover:scale-110"
             alt={product.title}
+            effect="blur"
+            src={product.image}
           />
-        </figure>
+        </div>
         <div className="flex h-28 flex-col items-start justify-between overflow-auto rounded-b-lg bg-gray-200 p-4 text-black dark:bg-gray-700 dark:text-gray-400">
           <span className="overflow-hidden text-start text-lg font-semibold">
             {product.title}
